ee8c49adb6b9e936d61aad6ee937df1747275c77
Mark Vayngrib <mark@tradle.io>
2017-04-03 14:12:44 -0400

    fix date of birth

--- index.js
+++ index.js
@@ -197,8 +197,8 @@ function createClient (opts) {
     }
 
     if (checkIdentity) {
-      const { dateOfBirth, addresses } = applicant.props
-      if (!dateOfBirth && addresses && addresses.length) {
+      const { date_of_birth, addresses } = applicant.props
+      if (!(date_of_birth && addresses && addresses.length)) {
         throw new Error('address is required for identity check')
       }
 
